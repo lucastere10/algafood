9ca8ffd3b734a5dbb6315d4f4359e969b313baca
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-01-26 16:41:10 -0300

    feat:  Arquitetura REST no projeto
    add: novas classes
    add: controllers para fazer as requisições do usuário
    add: service contendo a lógica de negócio da aplicação
    add: duas classes de exception para entidade em uso ou não encontrada

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,9 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -21,5 +24,5 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
-
+	
 }
